7923f35cef84a191dd0ce8d16917a9dabbe32e41
Dikran Hachikyan <dhachikian@expert-bg.org>
2025-01-29 10:22:20 +0200

    Python L01

---
+++ ex04_zan01.py
@@ -0,0 +1,32 @@
+# %%
+
+s1 = 'Lorem Ipsum is simply dummy'
+s2 = "Lorem Ipsum is simply dummy"
+
+# %%
+s3 = '''Lorem Ipsum is 
+simply dummy text of 
+the printing and 
+typesetting'''
+
+print(s1)
+print(s2)
+print(s3)
+# %%
+######################################
+# Function foo print "Hello Python"
+# Author: Me
+# Created: ...
+# ###################################
+def foo():
+    '''Function foo prints Hello Python
+    created: today
+    author: Me'''
+    print('Hello Python')
+
+# %%
+foo()
+# %%
+print(foo.__doc__)
+
+# %%
